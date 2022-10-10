1fcd94dd7d3c3fb62746a2f3ff6603d002c582f4
Documenter.jl <documenter@juliadocs.github.io>
2022-10-10 16:34:29 +0000

    build based on 404335c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sensemakr","category":"page"},{"location":"#Sensemakr","page":"Home","title":"Sensemakr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sensemakr.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sensemakr]","category":"page"}]
+}
