70ca39539cf82bba30eca5f10f92b71942e193c5
RodrigoGrijalba <ragrijalba119@gmail.com>
2022-10-17 14:31:01 -0500

    change const name

--- src/data.jl
+++ src/data.jl
@@ -1,7 +1,7 @@
 
-const path = joinpath(dirname(@__FILE__), "..", "data", "darfur.csv");
+const darfur_path = joinpath(dirname(@__FILE__), "..", "data", "darfur.csv");
 
 function load_darfur()
 
-    return CSV.read(path, DataFrame);
+    return CSV.read(darfur_path, DataFrame);
 end
